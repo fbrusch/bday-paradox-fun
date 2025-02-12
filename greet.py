d284e8fe14a902e7a642813552ddc630e6b0874c
fbrusch <fbrusch@gmail.com>
2025-02-12 13:01:49 +0000

    Add complexity

--- greet.py
+++ greet.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+print("Hello World!!!")
+
+from random import randint
+
+def generate_class(n):
+    return [randint(1,365) for _ in range(n)]
+    
+print("tell me how many people you want to generate!")
+n = input()
+n_number = int(n)
+
+print(generate_class(n_number))
+
+
